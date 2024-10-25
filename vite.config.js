d4d0ddf9454d7aa0d46362cf1f7ee8bf92cfc064
Viraj Gunjal <83692215+virajgunjal@users.noreply.github.com>
2024-10-25 16:35:27 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+
 
 export default defineConfig({
   server: {
     port: process.env.PORT || 3000, // fallback to port 3000 if PORT is not set
-  },plugins: [react()],
+  },
 });
-})
